82007872d891d0addaf2826d4de90433d4b46377
Jack Deadman <jackdeadman@ymail.com>
2014-05-05 19:49:19 +0100

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="site-footer">
+	<div class="footer-container">
+		<ul class="cf">
+			<li><a href="https://github.com/jackdeadman/blog">Source Code</a></li>
+			<li><a href="#">Feedback</a></li>
+		</ul>
+	</div>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,6 @@
+<header class="menu-container">
+	<ul class="menu-inside cf">
+		<li class="logo"><h2><a href="#">Blog</a></h2></li>
+		<li class="menu-icon"><a href="logout.php">Sign Out</a></li>
+	</ul>
+</header>
