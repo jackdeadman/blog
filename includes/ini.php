4967216ae16ce7547642cbc347c7f2a8293575e7
Jack Deadman <jackdeadman@ymail.com>
2014-05-05 18:26:46 +0100

    connection without password
    first commit of the initialising page without database password.

---
+++ includes/ini.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+require_once 'functions/general.php';
+
+$dbhost = "212.48.66.176";
+$dbuser = "itecdigi_417880c";
+$dbpass = "";
+$dbdata = "itecdigi_417880_sd1";
+
+$mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbdata);
+$GLOBALS['DB'] = $mysqli;
+if ($mysqli->connect_errno) {
+    die("Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
+}
+
+?>
