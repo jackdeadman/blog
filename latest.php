1ebfca8934f698c6f45b6bedb8f14b40d4883390
Jack Deadman <jackdeadman@ymail.com>
2014-05-05 18:30:11 +0100

    Source code link
    added source code link in the footer.

--- latest.php
+++ latest.php
@@ -113,7 +113,7 @@ require_once 'includes/checklogin.php';
 		<footer class="site-footer">
 			<div class="footer-container">
 				<ul class="cf">
-					<li><a href="#">Source Code</a></li>
+					<li><a href="https://github.com/jackdeadman/blog">Source Code</a></li>
 					<li><a href="#">Feedback</a></li>
 				</ul>
 			</div>
